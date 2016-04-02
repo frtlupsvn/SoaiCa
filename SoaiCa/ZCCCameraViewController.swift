afcdf8654a9198d1e1cc159c14f4d864ddae8226
Dzung Nguyen <dung.nguyen@rainmaker-labs.com>
2016-04-02 14:05:17 +0700

    no message

--- SoaiCa/ZCCCameraViewController.swift
+++ SoaiCa/ZCCCameraViewController.swift
@@ -30,54 +30,52 @@ class ZCCCameraViewController: UIViewController,UINavigationControllerDelegate, 
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func btnCalculateTapped(sender: AnyObject) {
+        self.detectFace(self.imageView.image!)
+    }
+    @IBAction func btnTakePhotoTapped(sender: AnyObject) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+       
+        imagePicker.dismissViewControllerAnimated(true) { () -> Void in
+           self.imageView.image = image
+        }
+        
+    }
+    
     func detectFace(image:UIImage) {
         
+        let ciImage = CIImage(CGImage: image.CGImage!)
         
-        imageView.image = image
+        let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)
         
-        let inputImage = CIImage(image: imageView.image!)!
+        let faces = faceDetector.featuresInImage(ciImage)
         
-        let detector = CIDetector(ofType: CIDetectorTypeFace,
-            context: context,
-            options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
-        
-        var faceFeatures: [CIFaceFeature]!
-        if let orientation: AnyObject = inputImage.properties[kCGImagePropertyOrientation as String] {
-            faceFeatures = detector.featuresInImage(inputImage, options: [CIDetectorImageOrientation: orientation]) as! [CIFaceFeature]
-        } else {
-            faceFeatures = detector.featuresInImage(inputImage)as! [CIFaceFeature]
+        if let face = faces.first as? CIFaceFeature {
+            print("Found face at \(face.bounds)")
+            
+            if face.hasLeftEyePosition {
+                print("Found left eye at \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Found right eye at \(face.rightEyePosition)")
+            }
+            
+            if face.hasMouthPosition {
+                print("Found mouth at \(face.mouthPosition)")
+            }
         }
-        
-        print(faceFeatures)
-        
-        // 1.
-        let inputImageSize = inputImage.extent.size
-        var transform = CGAffineTransformIdentity
-        transform = CGAffineTransformScale(transform, 1, -1)
-        transform = CGAffineTransformTranslate(transform, 0, -inputImageSize.height)
-        
-        for faceFeature in faceFeatures {
-            var faceViewBounds = CGRectApplyAffineTransform(faceFeature.bounds, transform)
-            
-            // 2.
-            let scale = min(imageView.bounds.size.width / inputImageSize.width,
-                imageView.bounds.size.height / inputImageSize.height)
-            let offsetX = (imageView.bounds.size.width - inputImageSize.width * scale) / 2
-            let offsetY = (imageView.bounds.size.height - inputImageSize.height * scale) / 2
-            
-            faceViewBounds = CGRectApplyAffineTransform(faceViewBounds, CGAffineTransformMakeScale(scale, scale))
-            faceViewBounds.origin.x += offsetX
-            faceViewBounds.origin.y += offsetY
-            
-            let faceView = UIView(frame: faceViewBounds)
-            faceView.layer.borderColor = UIColor.orangeColor().CGColor
-            faceView.layer.borderWidth = 2
-            
-            imageView.addSubview(faceView)
-        }
-
-        pixellated(image)
+       pixellated(image)
     }
+    
     
     func pixellated(image:UIImage) {
         // 1.
@@ -85,10 +83,10 @@ class ZCCCameraViewController: UIViewController,UINavigationControllerDelegate, 
         print(filter.attributes)
         let inputImage = CIImage(image: image)!
         filter.setValue(inputImage, forKey: kCIInputImageKey)
-        // filter.setValue(max(inputImage.extent.size.width, inputImage.extent.size.height) / 60, forKey: kCIInputScaleKey)
+        filter.setValue(max(inputImage.extent.size.width, inputImage.extent.size.height) / 60, forKey: kCIInputScaleKey)
         let fullPixellatedImage = filter.outputImage
-        // let cgImage = context.createCGImage(fullPixellatedImage, fromRect: fullPixellatedImage.extent)
-        // imageView.image = UIImage(CGImage: cgImage)
+         let cgImage = context.createCGImage(fullPixellatedImage!, fromRect: fullPixellatedImage!.extent)
+         imageView.image = UIImage(CGImage: cgImage)
         // 2.
         let detector = CIDetector(ofType: CIDetectorTypeFace,
             context: context,
@@ -138,31 +136,7 @@ class ZCCCameraViewController: UIViewController,UINavigationControllerDelegate, 
         let blendCGImage = context.createCGImage(blendOutputImage, fromRect: blendOutputImage.extent)
         imageView.image = UIImage(CGImage: blendCGImage)
     }
+    
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    @IBAction func btnTakePhotoTapped(sender: AnyObject) {
-        imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .PhotoLibrary
-        
-        presentViewController(imagePicker, animated: true, completion: nil)
-    }
-
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-       
-        imagePicker.dismissViewControllerAnimated(true) { () -> Void in
-            
-        }
-        self.detectFace(image)
-    }
     
 }
